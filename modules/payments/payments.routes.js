7c6ed50f58b359f2d964722dcc6ac21c89b5198d
federicosaenz013 <federicosaenz013@gmail.com>
2026-04-18 11:07:20 -0300

    Módulo 9: integración MercadoPago

---
+++ modules/payments/payments.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { createPreference, webhook } = require('./payments.controller');
+const { authenticateToken } = require('../../middleware/auth');
+
+router.post('/create-preference', authenticateToken, createPreference);
+router.post('/webhook', webhook);
+
+module.exports = router;
