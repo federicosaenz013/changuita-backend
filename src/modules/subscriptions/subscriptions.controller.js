6b01dd5bdb3918f2d87a7fa6196e52a397ee0e99
federicosaenz013 <federicosaenz013@gmail.com>
2026-05-15 00:06:03 -0300

    fix subscriptions controller content

--- src/modules/subscriptions/subscriptions.controller.js
+++ src/modules/subscriptions/subscriptions.controller.js
@@ -1,1 +1,24 @@
- 
+const subscriptionsService = require('./subscriptions.service');
+
+const getMyPlan = async (req, res, next) => {
+  try {
+    const data = await subscriptionsService.getPlan(req.user.id);
+    res.json(data);
+  } catch (err) { next(err); }
+};
+
+const getPlanes = async (req, res, next) => {
+  try {
+    res.json({ planes: subscriptionsService.PLANES });
+  } catch (err) { next(err); }
+};
+
+const subscribeToPlan = async (req, res, next) => {
+  try {
+    const { plan, mp_payment_id, periodo } = req.body;
+    const data = await subscriptionsService.updatePlan(req.user.id, plan, mp_payment_id, periodo);
+    res.json({ message: 'Plan actualizado', ...data });
+  } catch (err) { next(err); }
+};
+
+module.exports = { getMyPlan, getPlanes, subscribeToPlan };
